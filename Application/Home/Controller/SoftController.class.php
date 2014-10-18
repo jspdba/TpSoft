de5dc69624f98d4f7d396ac06b962ed1eb14126e
jspdba <jspdba@163.com>
2014-10-18 23:50:59 +0800

    前台去掉了修改，删除软件的权限

--- Application/Home/Controller/SoftController.class.php
+++ Application/Home/Controller/SoftController.class.php
@@ -37,38 +37,6 @@ class SoftController extends Controller{
         $this->display(); // 输出模板
 	}
 
-	public function insert(){
-//		$entity=M('Soft','think_','DB_CONFIG');
-		$entity=M('Soft');
-		if($entity->create($_POST,1)){
-			if($result=$entity->add()){
-				$this->success('新增成功', 'index');
-			}
-		}
-		$this->error('新增失败,'.$entity->getDbError(),true);
-	}
-	//稍后改名为input
-	public function add(){
-		$entity=M('Topic');
-		#$result=$entity->where("id=1 ")->find();//只查询一条
-		$list=$entity->field("id,name")->select();
-
-		if($list){
-			#dump($list);
-			$this->assign("list",$list);
-		}else{
-			print_r($entity->getDbError());
-		}
-		$this->display();
-	}
-	public function delete(){
-		$entity=M('Soft');
-		$id=I("id");
-		if($ct=$entity->delete($id)){
-			$this->success('删除成功!'.'删除'.$ct.'条', 'index');
-		}
-		$this->error('删除失败'.$entity->getDbError(),'index');
-	}
     //详细信息页面
     public function info(){
         $id=I('id');
--- Application/Home/Controller/TopicController.class.php
+++ Application/Home/Controller/TopicController.class.php
@@ -19,27 +19,4 @@ class TopicController extends  Controller {
         }
         $this->display();
     }
-
-    public function input(){
-        $this->display();
-    }
-
-    public function insert(){
-        //		$entity=M('Soft','think_','DB_CONFIG');
-        $entity=M('Topic');
-        if($entity->create($_POST,1)){
-            if($result=$entity->add()){
-                $this->success('新增成功', 'index');
-            }
-        }
-        $this->error('新增失败,'.$entity->getDbError(),true);
-    }
-    public function delete(){
-        $entity=M('Topic');
-        $id=I("id");
-        if($ct=$entity->delete($id)){
-            $this->success('删除成功!'.'删除'.$ct.'条', 'index');
-        }
-        $this->error('删除失败'.$entity->getDbError(),'index');
-    }
 } 
